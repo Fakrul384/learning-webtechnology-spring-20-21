aa70035f95e3dd44a18f97b69cad5f9be6528b97
unknown <hridoy.fokhrul@gmail.com>
2021-02-16 09:31:10 +0600

    New file added to the folder

---
+++ MID_LAB_TASK_PHP_04/p_1.php
@@ -0,0 +1,10 @@
+<?php 
+            $length=8;
+            $width=9;
+
+            $area  = $length * $width;
+            $perimeter= 2 * ($length + $width);
+
+            echo "The Area is  : ".$area."<br/>"; 
+            echo "The Perimeter is  : ".$perimeter;
+?>
