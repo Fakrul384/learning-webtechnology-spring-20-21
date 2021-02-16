15c356d2d55c6011faec9e6774a073d76f3a6f75
unknown <hridoy.fokhrul@gmail.com>
2021-02-16 10:54:29 +0600

    new file added to the folder

---
+++ MID_LAB_TASK_PHP_04/p_6.php
@@ -0,0 +1,11 @@
+<?php
+$value=array("Fakrul","Hridoy","Amin","saif","Rabbi");
+
+
+if(in_array("Hridoy",$value)){
+echo "Hridoy is there inside the array";}
+
+else
+{ echo "Hridoy is not there ";}
+
+?>
