8c21e6a753358eaaaffc7f22f8c2a55424f6ddd1
unknown <hridoy.fokhrul@gmail.com>
2021-02-16 10:08:30 +0600

    new file added to the folder

---
+++ MID_LAB_TASK_PHP_04/p_4.php
@@ -0,0 +1,17 @@
+<?php
+    $num1=30;
+    $num2=20;
+    $num3=10;
+    if($num1>$num2 and $num1>$num3)
+    {
+        echo "Max is $num1";
+    }
+    else if($num2>$num3)
+    {
+        echo "Max is $num2";
+    }
+    else
+    {
+        echo "Max is $num3";
+    }
+?>
